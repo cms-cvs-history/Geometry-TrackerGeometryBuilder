ab89163b6584449dd61ce0bd292239a13de5770a
Mike Case <sha1-1d692184473970509f0e165e4d62c8c025ba163f@cern.ch>
2008-04-18 15:01:06 +0000

    files to demonstrate working tracker geometry.

---
+++ test/conddbtest.sh
@@ -0,0 +1,9 @@
+date
+cmsRun ../data/trackerGeometricDetLoaderSQLITE.cfg
+date
+cmsRun OnlyTrackerChainExampleSim.cfg
+date
+cmsRun OnlyTrackerChainExampleReco.cfg
+date
+cmsRun OnlyTrackerChainExampleRecoDB.cfg
+date
