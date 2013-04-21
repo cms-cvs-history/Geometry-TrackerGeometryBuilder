21ceed738462f7b900955fc70bcf142cda44fa01
cmsbuild <>
2013-04-21 20:41:43 +0000

    This commit was manufactured by cvs2git to create tag 'Geometry- TrackerGeometryBuilder-CMSSW_6_1_2_SLHC4_patch1'.
    Sprout from master 2013-04-21 20:41:36 UTC David Lange <sha1-b4ab71fd0383141ecc8720f477f1ba20f2889d65@cern.ch> 'fix some memory leaks'
    Cherrypick from master 2012-09-22 08:09:45 UTC Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch> 'use new diff method in topology':
        interface/PlaneBuilderForGluedDet.h
        interface/ProxyPixelTopology.h
        interface/ProxyStripTopology.h
        src/PlaneBuilderForGluedDet.cc
        src/PlaneBuilderFromGeometricDet.cc
        test/TrackerDigiGeometryAnalyzer.cc

--- interface/ProxyPixelTopology.h
+++ interface/ProxyPixelTopology.h
@@ -18,9 +18,9 @@
 ///
 ///  \author    : Andreas Mussgiller
 ///  date       : December 2010
-///  $Revision: 1.9 $
-///  $Date: 2012/09/15 16:31:43 $
-///  (last update by $Author: innocent $)
+///  $Revision: 1.8 $
+///  $Date: 2012/07/05 13:32:03 $
+///  (last update by $Author: yana $)
 
 #include "DataFormats/GeometryCommonDetAlgo/interface/DeepCopyPointerByClone.h"
 
@@ -28,12 +28,12 @@
 #include "Geometry/CommonTopologies/interface/PixelTopology.h"
 #include "Geometry/TrackerGeometryBuilder/interface/PixelGeomDetType.h"
 
-class Plane;
+class BoundPlane;
 
 class ProxyPixelTopology GCC11_FINAL : public PixelTopology {
 public:
 
-  ProxyPixelTopology( PixelGeomDetType* type, Plane * bp );
+  ProxyPixelTopology( PixelGeomDetType* type, BoundPlane * bp );
 
   virtual LocalPoint localPosition( const MeasurementPoint& ) const;
   /// conversion taking also the predicted track state 
